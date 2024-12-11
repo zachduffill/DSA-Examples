dd5b32f9ffdb681a44d405b27e4e633c30456dbd
zachduffill <zachdach321@googlemail.com>
2024-12-11 16:51:34 +0000

    Added MainMenu Display() and HandleInput() funcs Added program.cs main loop

--- MainMenu.cs
+++ MainMenu.cs
@@ -5,21 +5,29 @@ namespace DSA_Examples
     internal class MainMenu
     {
         private MenuItem[] MenuItems { get; init; }
+        private int MenuOffset { get; set; } 
 
-        private class MenuItem
+        public class MenuItem
         {
             public string Name { get; init; }
+            public Type Type { get; init; }
 
-            public MenuItem(string name)
+            public MenuItem(string name, Type type)
             {
                 Name = name;
+                Type = type;
+            }
+
+            public override string ToString()
+            {
+                return Name;
             }
         }
 
         public MainMenu()
         {
             MenuItems = LoadExamples();
-            Console.WriteLine(MenuItems);
+            MenuOffset = 0;
         }
 
         static private MenuItem[] LoadExamples()
@@ -32,13 +40,41 @@ namespace DSA_Examples
 
             for (int i = 0; i < menuItems.Count(); i++)
             {
-                menuItems[i] = new MenuItem(types[i].Name);
+                menuItems[i] = new MenuItem(types[i].Name, types[i]);
             }
 
             return menuItems;
         }
 
         public void Display()
+        {
+            Console.Clear();
+            Console.WriteLine("DSA-Examples");
+            Console.WriteLine("------------");
+            Console.WriteLine();
+            for (int i = 0; i < MenuItems.Count(); i++)
+            {
+                Console.WriteLine($"[{i}] {MenuItems[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("[x] Exit");
+        }
+
+        public MenuItem? HandleInput()
+        {
+            ConsoleKeyInfo input = new ConsoleKeyInfo();
+            while (!Char.IsDigit(input.KeyChar))
+            {
+                input = Console.ReadKey(true);
+                Console.WriteLine(input);
+                if (input.Key == ConsoleKey.UpArrow) Navigate(1);
+                else if (input.Key == ConsoleKey.DownArrow) Navigate(-1);
+                else if (input.Key == ConsoleKey.X) return null;
+            }
+            return MenuItems[int.Parse(input.KeyChar.ToString())+MenuOffset];
+        }
+
+        private void Navigate(int direction)
         {
 
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DSA_Examples;
+using static DSA_Examples.MainMenu;
 
-MainMenu menu = new MainMenu(); 
+MainMenu menu = new MainMenu();
+while (true)
+{
+    menu.Display();
+    MenuItem? selectedItem = menu.HandleInput(); // if user presses exit key, null item is returned,
+    if (selectedItem == null) return 0; // and program exits
+
+
+}
