e83d92df5b5ca4d32aa20ed0710ddb3bce298540
zachduffill <zachdach321@googlemail.com>
2024-12-13 16:52:36 +0000

    Finished Queue

--- Examples/Queue.cs
+++ Examples/Queue.cs
@@ -51,21 +51,50 @@
             }
         }
 
-        public Queue()
+        public object Peek()
         {
-            queue = new LinkedList([1,2,3,4,5,6]);
+            if (queue.Head == null) return -1;
+            return queue.Head.Value;
+        }
 
-            Tests = new Test<object?>[]
+        public object Enqueue(object obj)
+        {
+            LinkedList.Node node = new LinkedList.Node(obj);
+            if (queue.Head == null || queue.Tail == null) queue.Head = queue.Tail = node;
+            else
             {
-                new Test<object?>(() => ToString(),0)
-            };
+                queue.Tail.Next = node;
+                node.Prev = queue.Tail;
+                queue.Tail = node;
+            }
+
+            return queue.Tail.Value;
+        }
+
+        public object? Dequeue()
+        {
+            if (queue.Head == null || queue.Tail == null) return null;
+
+            LinkedList.Node node = queue.Head;
+
+            if (node.Next == null)
+            {
+                queue.Head = queue.Tail = null;
+            }
+            else
+            {
+                node.Next.Prev = null;
+                queue.Head = node.Next;
+            }
+
+            return node.Value;
         }
 
         public override string ToString()
         {
             if (queue.Head == null || queue.Tail == null) return "";
-            
-            string str = ""; 
+
+            string str = "";
 
             LinkedList.Node? curr = queue.Head;
             while (curr != queue.Tail && curr != null)
@@ -76,6 +105,19 @@
             str += $"[{queue.Tail.Value}]";
 
             return str;
+        }
+
+        public Queue()
+        {
+            queue = new LinkedList([1,2,3,4,5,6]);
+
+            Tests = new Test<object?>[]
+            {
+                new Test<object?>(() => Peek(),1),
+                new Test<object?>(() => Dequeue(),1),
+                new Test<object?>(() => Enqueue(0),0),
+                new Test<object?>(() => ToString(),"[2] -> [3] -> [4] -> [5] -> [6] -> [0]"),
+            };
         }
     }
 }
