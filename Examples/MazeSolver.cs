dbe93993dd1d9b3a816106fc11633c15be314574
zachduffill <zachdach321@googlemail.com>
2024-12-19 10:02:22 +0000

    Finished recursive MazeSolver and tests successful

--- Examples/MazeSolver.cs
+++ Examples/MazeSolver.cs
@@ -8,25 +8,43 @@ namespace DSA_Examples.Examples
         private int width => (int)Math.Sqrt(maze.Length);
         private int height => width;
 
-        private struct Coord { public int x; public int y; }
+        public struct Coord 
+        { 
+            public int x; public int y;
+
+            public override string ToString()
+            {
+                return $"[{x}, {y}]";
+            }
+        }
 
         public Coord[] Solve()
         {
-            Coord[] path = new Coord[width*height];
-            Coord[] seen = new Coord[width*height];
-            Coord start = new Coord { x=0,y=0 };
+            Stack<Coord> path = new Stack<Coord>();
+            Stack<Coord> seen = new Stack<Coord>();
+            Coord start = new Coord { x = 0, y = 0 };
+            Coord[] dirs = new Coord[4] { new Coord { x = 0, y = 1 }, new Coord { x = 1, y = 0 }, new Coord { x = 0, y = -1 }, new Coord { x = -1, y = 0 } };
 
             bool walk(Coord pos)
             {
                 if (seen.Contains(pos)) return false;
 
-                seen.Append(pos);
-                path.Append(pos);
+                seen.Push(pos);
+                path.Push(pos);
 
                 if (maze[pos.x, pos.y] == Cell.End) return true;
-                if (maze[pos.x, pos.y] == Cell.Wall) return false;
+                if (maze[pos.x, pos.y] == Cell.Wall)
+                {
+                    path.Pop();
+                    return false;
+                }
+                    
+                foreach (Coord dir in dirs)
+                {
+                    if (walk(new Coord() { x = pos.x + dir.x, y = pos.y + dir.y })) return true;
+                }
 
-
+                return false;
             }
 
             for (int i = 0; i < height; i++)
@@ -38,7 +56,7 @@ namespace DSA_Examples.Examples
             }
 
             walk(start);
-            return path;
+            return path.Reverse().ToArray();
         }
 
         public override string ToString()
@@ -73,7 +91,25 @@ namespace DSA_Examples.Examples
 
             Tests = new Test<object?>[]
             {
-                new Test<object?>("ToString",() => ToString(),null)
+                new Test<object?>("ToString",() => ToString(),null),
+                new Test<object?>("Solve",() => Solve(), new Coord[]
+                {
+                    new Coord { x = 1, y = 1 },
+                    new Coord { x = 1, y = 2 },
+                    new Coord { x = 1, y = 3 },
+                    new Coord { x = 2, y = 3 },
+                    new Coord { x = 3, y = 3 },
+                    new Coord { x = 3, y = 4 },
+                    new Coord { x = 3, y = 5 },
+                    new Coord { x = 4, y = 5 },
+                    new Coord { x = 5, y = 5 },
+                    new Coord { x = 5, y = 6 },
+                    new Coord { x = 5, y = 7 },
+                    new Coord { x = 5, y = 8 },
+                    new Coord { x = 6, y = 8 },
+                    new Coord { x = 7, y = 8 },
+                    new Coord { x = 8, y = 8 }
+                }),
             };
         }
     }
