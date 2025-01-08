1cbcf9d2be8ef9ddbba6552f68fe99e612f3701a
zachduffill <zachdach321@googlemail.com>
2025-01-08 17:21:13 +0000

    Commented MazeSolver

--- Algorithms/MazeSolver.cs
+++ Algorithms/MazeSolver.cs
@@ -5,7 +5,7 @@ namespace DSA_Examples.Examples
 {
     internal class MazeSolver : Example
     {
-        private enum Cell { Empty = ' ', Wall = '#', Start = 'S', End = 'E' }
+        private enum Cell { Empty = ' ', Wall = '#', Start = 'S', End = 'E' } // Symbol definitions for Cell types
         private Cell[,] maze;
         private int width => (int)Math.Sqrt(maze.Length);
         private int height => width;
@@ -20,14 +20,14 @@ namespace DSA_Examples.Examples
             }
         }
 
-        public Coord[] Solve()
+        public Coord[] Solve() // Finds a valid path from start to end for maze, returns path as array of Coords        O(n^2)   
         {
             Stack<Coord> path = new Stack<Coord>();
             Stack<Coord> seen = new Stack<Coord>();
             Coord start = new Coord { x = 0, y = 0 };
             Coord[] dirs = new Coord[4] { new Coord { x = 0, y = 1 }, new Coord { x = 1, y = 0 }, new Coord { x = 0, y = -1 }, new Coord { x = -1, y = 0 } };
 
-            bool walk(Coord pos)
+            bool walk(Coord pos) // Recursive function to walk the maze - Returns false for dead ends
             {
                 if (seen.Contains(pos)) return false;
 
@@ -49,7 +49,7 @@ namespace DSA_Examples.Examples
                 return false;
             }
 
-            for (int i = 0; i < height; i++)
+            for (int i = 0; i < height; i++) // Find start position
             {
                 for (int j = 0; j < width; j++)
                 {
