cdd295415879af376d5c65c4e23dbdfa861172ef
zachduffill <zachdach321@googlemail.com>
2025-01-14 09:03:48 +0000

    Moved Node class to own file

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -6,25 +6,6 @@
         public Node? Tail { get; set; }
         public int Length { get; set; }
 
-        public class Node // class representing a singular node in the list
-        {
-            public object Value { get; set; }
-            public Node? Next { get; set; }
-            public Node? Prev { get; set; }
-
-            public Node(object val, Node? next = null, Node? prev = null) // instantiates a node with a given value
-            {
-                Value = val;
-                Next = next;
-                Prev = prev;
-            }
-
-            public override string ToString()
-            {
-                return this.Value.ToString() ?? "Null";
-            }
-        }
-
         public LinkedList(object[] initialValues) // Fill the list with values defined in the object[] argument
         {
             if (initialValues.Length == 0) return;
---
+++ DataStructures/Node.cs
@@ -0,0 +1,21 @@
+﻿namespace DSA_Examples.Utility
+{
+    public class Node // class representing a singular node in the list
+    {
+        public object Value { get; set; }
+        public Node? Next { get; set; }
+        public Node? Prev { get; set; }
+
+        public Node(object val, Node? next = null, Node? prev = null) // instantiates a node with a given value
+        {
+            Value = val;
+            Next = next;
+            Prev = prev;
+        }
+
+        public override string ToString()
+        {
+            return this.Value.ToString() ?? "Null";
+        }
+    }
+}
