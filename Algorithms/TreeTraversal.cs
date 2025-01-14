709d85c8e23ac0618321f81ee91c5529b7c13d8b
zachduffill <zachdach321@googlemail.com>
2025-01-14 11:05:47 +0000

    Created TreeTraversal

---
+++ Algorithms/TreeTraversal.cs
@@ -0,0 +1,15 @@
+﻿
+using DSA_Examples.Utility;
+
+namespace DSA_Examples.Examples
+{
+    internal class TreeTraversal : Example
+    {
+        Tree tree;
+
+        public TreeTraversal()
+        {
+            tree = new Tree();
+        }
+    }
+}
